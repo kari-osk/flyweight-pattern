ee09435a883771900d4b2d5d43ff6a4509eef5a4
kari-osk <karinaosuka@gmail.com>
2022-03-07 10:35:50 -0300

    Initial commit

---
+++ src/service/FlyweightFactory.java
@@ -0,0 +1,31 @@
+package service;
+
+import model.Quadrado;
+import model.Triangulo;
+
+import java.util.HashMap;
+
+public class FlyweightFactory {
+    // final é como constante e não variavel
+    public static final HashMap<String, Triangulo> trianguloMap = new HashMap<>();
+
+    public static Triangulo obterTriangulo(String cor) {
+        Triangulo triangulo = trianguloMap.get(cor);
+        if (triangulo == null) {
+            triangulo = new Triangulo(cor);
+            trianguloMap.put(cor, triangulo);
+        }
+        return triangulo;
+    }
+
+    public static final HashMap<Integer, Quadrado> quadradoMap = new HashMap<>();
+
+    public static Quadrado obterQuadrado(int tamanho) {
+        Quadrado quadrado = quadradoMap.get(tamanho);
+        if(quadrado == null) {
+            quadrado = new Quadrado(tamanho);
+            quadradoMap.put(tamanho, quadrado);
+        }
+        return quadrado;
+    }
+}
